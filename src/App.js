42acd61988ece9fc24b39e39730de8a3adbdf250
megamanxl <alexander.lofqvist@gmail.com>
2022-11-03 08:34:10 +0100

    Added period to example projects

--- src/App.js
+++ src/App.js
@@ -63,20 +63,20 @@ function App() {
                   flexWrap: 'wrap',
                   alignItems: 'center'
                 }}>
-                  <ClickShowHideSpanIcon content={['IoT access control systems', 'Cloud service with automated CI', 'Upgrade service for cloud connected devices']} src={golang} alt={golang} />
-                  <ClickShowHideSpanIcon content={['Backend for video hosting system in the cloud']} src={nodejs} alt="nodejs" />
-                  <ClickShowHideSpanIcon content={['A multi-user platform for analysis of proteomics data', 'Processing of sensor readings from sensors in a production line factory']} src={java} alt="java" />
-                  <ClickShowHideSpanIcon content={['Web application for planning surveillance']} src={sproutcore} alt="sproutcore" />
-                  <ClickShowHideSpanIcon content={['Platform for building business intelligence solutions', 'Web interface for command line tools in a cluster environment']} src={js} alt="js" />
-                  <ClickShowHideSpanIcon content={['Keyless security']} src={cpp} alt="cpp" />
-                  <ClickShowHideSpanIcon content={['Sensor surveillance', 'Device automation']} src={freertos} alt="freertos" />
-                  <ClickShowHideSpanIcon content={['Temperature control of devices', 'Motor control for devices', 'Image processing', 'Camera motor control', 'BLE IoT solution for monitoring of combustion engines', 'Train signal simulator for simulation of train traffic']} src={c} alt="c" />
-                  <ClickShowHideSpanIcon content={['Time reporting application']} src={reactjs} alt="reactjs" />
-                  <ClickShowHideSpanIcon content={['Evolutionary particle swarm optimization of time scheduling']} src={python} alt="reactjs" />
-                  <ClickShowHideSpanIcon content={['Data-driven military planning, crisis situation assessment and risk calculation', 'E-Shop with payment services through Debitech', 'Automatic test environment']} src={angular} alt="angular" />
-                  <ClickShowHideSpanIcon content={['Webshop for a circular economy clothing shop']} src={vue} alt="vue" />
-                  <ClickShowHideSpanIcon content={['IoT solution for urban farming environments']} src={raspberrypie} alt="raspberrypie" />
-                  <ClickShowHideSpanIcon content={['Solar charger for android tablets']} src={android} alt="android" />
+                  <ClickShowHideSpanIcon content={['IoT access control systems.', 'Cloud service with automated CI.', 'Upgrade service for cloud connected devices.']} src={golang} alt={golang} />
+                  <ClickShowHideSpanIcon content={['Backend for video hosting system in the cloud.']} src={nodejs} alt="nodejs" />
+                  <ClickShowHideSpanIcon content={['A multi-user platform for analysis of proteomics data.', 'Processing of sensor readings from sensors in a production line factory.']} src={java} alt="java" />
+                  <ClickShowHideSpanIcon content={['Web application for planning surveillance.']} src={sproutcore} alt="sproutcore" />
+                  <ClickShowHideSpanIcon content={['Platform for building business intelligence solutions.', 'Web interface for command line tools in a cluster environment.']} src={js} alt="js" />
+                  <ClickShowHideSpanIcon content={['Keyless security.']} src={cpp} alt="cpp" />
+                  <ClickShowHideSpanIcon content={['Sensor surveillance.', 'Device automation.']} src={freertos} alt="freertos" />
+                  <ClickShowHideSpanIcon content={['Temperature control of devices.', 'Motor control for devices.', 'Image processing.', 'Camera motor control.', 'BLE IoT solution for monitoring of combustion engines.', 'Train signal simulator for simulation of train traffic.']} src={c} alt="c" />
+                  <ClickShowHideSpanIcon content={['Time reporting application.']} src={reactjs} alt="reactjs" />
+                  <ClickShowHideSpanIcon content={['Evolutionary particle swarm optimization of time scheduling.']} src={python} alt="python" />
+                  <ClickShowHideSpanIcon content={['Data-driven military planning, crisis situation assessment and risk calculation.', 'E-Shop with payment services through Debitech.', 'Automatic test environment.']} src={angular} alt="angular" />
+                  <ClickShowHideSpanIcon content={['Webshop for a circular economy clothing shop.']} src={vue} alt="vue" />
+                  <ClickShowHideSpanIcon content={['IoT solution for urban farming environments.']} src={raspberrypie} alt="raspberrypie" />
+                  <ClickShowHideSpanIcon content={['Solar charger for android tablets.']} src={android} alt="android" />
                 </FadeInDiv>
               </Footer>
             </Grid>
